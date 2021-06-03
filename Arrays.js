cd6fa140b0ca4684214261c950c07b164597d25e
Siri Lalitha <sirilalitha9088@gmail.com>
2021-06-03 17:35:36 +0530

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,19 @@
+function getSecondLargest(nums) {
+    // Complete the function
+    nums.sort((a, b) => a < b); // This sorts inversely
+    var a = nums.shift();
+
+    while (a == nums[0]) {
+        a = nums.shift();
+    }
+    a = nums.shift();
+
+    return a;
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
