6a1dfb568647f4b75a5e9e992e1da34d3ef769b3
Siri Lalitha <sirilalitha9088@gmail.com>
2021-06-03 17:39:26 +0530

    Arrays

--- Day-03/Arrays.js
+++ Day-03/Arrays.js
@@ -1,16 +1,12 @@
 function getSecondLargest(nums) {
     // Complete the function
-    nums.sort((a, b) => a < b); // This sorts inversely
-    var a = nums.shift();
+    var sorted_array = nums.sort(function (a,b) {return a - b;});
+    var unique_sorted_array = sorted_array.filter(function(elem, index, self) {
+        return index === self.indexOf(elem);
+    })
 
-    while (a == nums[0]) {
-        a = nums.shift();
-    }
-    a = nums.shift();
-
-    return a;
+    return unique_sorted_array[unique_sorted_array.length - 2];
 }
-
 function main() {
     const n = +(readLine());
     const nums = readLine().split(' ').map(Number);
