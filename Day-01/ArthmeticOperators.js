bdda3f4c64e02c40f1e883c42465e279faeac1c2
Siri Lalitha <sirilalitha9088@gmail.com>
2021-06-03 14:53:01 +0530

    Create ArthmeticOperators.js

---
+++ Day-01/ArthmeticOperators.js
@@ -0,0 +1,31 @@
+
+// ========================
+//         Solution
+// ========================
+
+// Calculate the area of a rectangle.
+// length: The length of the rectangle.
+// Width: The width of the rectangle.
+// Return a number denoting the rectangle's area.
+
+function getArea(length, width) {
+    return length*width;
+}
+
+// Calculate the perimeter of a rectangle.
+// length: The length of the rectangle.
+// Width: The width of the rectangle.
+// Return a number denoting the perimeter of a rectangle.
+
+function getPerimeter(length, width) {
+    // Write your code here
+    return 2*(length+width);
+}
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
