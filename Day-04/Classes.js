c471a341b0372691f5515708b2271bc9582f0867
Siri Lalitha <sirilalitha9088@gmail.com>
2021-06-03 17:53:26 +0530

    Classes

---
+++ Day-04/Classes.js
@@ -0,0 +1,20 @@
+
+class Polygon {
+    constructor(sides) {
+        this.perimeter = function() {
+            var sum = 0;
+            sides.forEach((value) => {
+                sum += value;
+            });
+            return sum;
+        }
+    }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
